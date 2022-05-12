70dd1e9aa62a637a2ee14e3fb21c8dd26372dd5c
Chrystian Raony Perazzoli <chrystian.raony@gmail.com>
2022-05-12 19:48:54 -0300

    Projeto Finalizado

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    /*{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Bento',
+                partido: 'Aberto',
+                fotos: [
+                    { url: 'bento.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77777',
+                nome: 'Bento',
+                partido: 'Aberto',
+                fotos: [
+                    { url: 'bento.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },*/
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos : [
+            {
+                numero: '99',
+                nome: 'Miguel',
+                partido: 'ABC',
+                vice: 'Bento',
+                fotos:[
+                    {url:'miguel.jpg', legenda: 'Prefeito'},
+                    {url:'bento.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Bento',
+                partido: 'ABC',
+                vice: 'Miguel',
+                fotos: [
+                    {url:'bento.jpg', legenda:'Prefeito'},
+                    {url:'miguel.jpg', legenda:'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
